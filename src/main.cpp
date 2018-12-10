fbd29f663c43ff860d4ecd22ebdac848d905a7cb
Saimouli <ksaimouli@gmail.com>
2018-12-10 16:00:01 -0500

    [src/main][Task 2.8]: Implemented userinterface which will take the userchoice for the turtlebot to explore the map using spiral or linear trajectories

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,8 @@
  *@copyright MIT License
  *@brief main function
  */
-# include <frontier_exploration_turtlebot/Guidance.h>
+#include <frontier_exploration_turtlebot/PathPlanning.h>
+
 
 /**
  * @brief      main function
@@ -39,7 +40,39 @@
  * @return     int of value zero
  */
 int main(int argc, char* argv[]) {
+// Initialize the ros node
+  ros::init(argc, argv, "frontierExplorer");
+
+  int userChoice;
+  std::cout << "Welcome to Turtlebot Explorer" << std::endl;
+  std::cout << "Once you are satisfied with the map press (ctr+c)" << std::endl;
+  std::cout << "To save the map rosrun map_server map_saver -f my_map"
+            << std::endl;
+  std::cout
+  << "Would you like to take linear path (0) or spiral path finder (1)?"
+      " (Enter 0 or 1): ";
+  std::cin >> userChoice;
+
+// Create the PathPlanning object
+  PathPlanning pathPlanning;
+
+  if (userChoice == 0) {
+    // Run the PathPlanning behaviour
+    while (ros::ok()) {
+      pathPlanning.linearPathGenerator();
+    }
+  }
+std::cin.get();
+  if (userChoice == 1) {
+    while (ros::ok()) {
+      pathPlanning.spiralPathGenerator();
+    }
+  } else {
+    ROS_WARN_ONCE("Applying linearPathGenerator by default");
+    while (ros::ok()) {
+      pathPlanning.linearPathGenerator();
+    }
+  }
 
   return 0;
 }
-
